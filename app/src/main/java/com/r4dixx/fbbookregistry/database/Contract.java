6c4789274d72d324666baf05be2cda40a979908d
r4dixx <amael.sikel@gmail.com>
2018-07-23 22:44:51 +0200

    :construction: Contract class

--- app/src/main/java/com/r4dixx/fbbookregistry/database/Contract.java
+++ app/src/main/java/com/r4dixx/fbbookregistry/database/Contract.java
@@ -12,14 +12,35 @@ public final class Contract {
         public static final String TABLE_NAME = "books";
 
         public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_BREED = "breed";
-        public static final String COLUMN_GENDER = "gender";
-        public static final String COLUMN_WEIGHT = "weight";
+        public static final String COLUMN_TITLE = "title";
+        public static final String COLUMN_AUTHOR = "author";
+        public static final String COLUMN_EDITION = "edition";
+        public static final String COLUMN_YEAR = "year";
+        public static final String COLUMN_SUBJECT = "subject";
 
-        public static final int GENDER_UNKNOWN = 0;
-        public static final int GENDER_MALE = 1;
-        public static final int GENDER_FEMALE = 2;
+        public static final int SUBJECT_UNKNOWN = 0;
+
+        public static final int SUBJECT_ALCHEMY = 1;
+        public static final int SUBJECT_ANCIENT_STUDIES = 2;
+        public static final int SUBJECT_APPARITION = 3;
+        public static final int SUBJECT_ARITHMANCY = 4;
+        public static final int SUBJECT_ART = 5;
+        public static final int SUBJECT_ASTRONOMY = 6;
+        public static final int SUBJECT_CHARMS = 7;
+        public static final int SUBJECT_CREATURES = 8;
+        public static final int SUBJECT_DARK_ARTS = 9;
+        public static final int SUBJECT_DIVINATION = 10;
+        public static final int SUBJECT_FLYING = 11;
+        public static final int SUBJECT_HERBOLOGY = 12;
+        public static final int SUBJECT_MAGIC_HISTORY = 13;
+        public static final int SUBJECT_MAGICAL_THEORY = 14;
+        public static final int SUBJECT_MUGGLES_STUDY = 15;
+        public static final int SUBJECT_MUGGLE_ART = 16;
+        public static final int SUBJECT_MUSIC = 17;
+        public static final int SUBJECT_POTIONS = 18;
+        public static final int SUBJECT_ANCIENT_RUNES = 19;
+        public static final int SUBJECT_TRANFIGURATION = 20;
+        public static final int SUBJECT_XYLOMANCY = 21;
 
     }
 }
